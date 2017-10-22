95a7181cc92606322f8bc44f857fa44be82eca28
bupuxiang <bupuxiang@qq.com>
2017-10-22 22:15:15 +0800

    add sime files.

--- Code/Java/thread/Ball.java
+++ Code/Java/thread/Ball.java
@@ -2,7 +2,46 @@ package thread;
 
 import java.awt.geom.*;
 
+/**
+ * A ball that moves and bounds off the edges of a rectangle
+ * @version 1.33 2007-05-17
+ * @author Cay Horstman
+ */
 public class Ball
 {
-    
+    private static final int XSIZE = 15;
+    private static final int YSIZE = 15;
+    private double x = 0;
+    private double y = 0;
+    private double dx = 1;
+    private double dy = 1;
+
+    /**
+     * Moves the ball to next position, reversing direction if it hits one of the edges
+     */
+    public void move(Rectangle2D bounds) {
+        y += dx;
+        y += dy;
+        if (x < bounds.getMinX()) {
+            x = bounds.getMinX();
+            dx = -dx;
+        }
+        if(x + XSIZE >= bounds.getMaxX()) {
+            x = bounds.getMaxX() - XSIZE;
+            dx = -dx;
+        }
+
+        if (y < bounds.getMinY()) {
+            y = bounds.getMinY();
+            dy = -dy;
+        }
+        if (y + YSIZE >= bounds.getMaxY()) {
+            y = bounds.getMaxY() - YSIZE;
+            dy = -dy;
+        }
+    }
+
+    public Ellipse2D getShape() {
+        return new Ellipse2D.Double(x, y, XSIZE, YSIZE);
+    }
 }
