7b366ff2d928138e945926957f7a7788d1afaa76
bupuxiang <bupuxiang@vip.qq.com>
2017-11-17 09:23:32 +0800

    sss

---
+++ Code/Java/CalendarTest.java
@@ -0,0 +1,41 @@
+import java.time.*;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+/**
+ * @version 1
+ * @author Bu Puxiang
+ */
+public class CalendarTest{
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int today = date.getDayOfMonth();
+
+        date = date.minusDays(today - 1); // Set to start of month
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.getValue();
+
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for (int i = 1; i < value; i++) {
+            System.out.print("    ");
+        }
+
+        while(date.getMonthValue() == month) {
+            System.out.printf("%3d", date.getDayOfMonth());
+            if (date.getDayOfMonth() == today) {
+                System.out.print("*");
+            }else {
+                System.out.print(" ");
+            }
+            date = date.plusDays(1);
+            if (date.getDayOfWeek().getValue() == 1) {
+                System.out.println();
+            }
+        }
+        if (date.getDayOfWeek().getValue() != 1) {
+            System.out.println();
+        }
+    }
+}
